4da387a8000015b12b244d4e8b9d1510cad5de2c
fayoz <fayoz@fayoz.fayoz>
2025-11-30 00:23:43 -0500

    loader added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center items-center min-h-[200px]">
+      <div className="w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
 };
 
 export default Loader;
